16dfbae1c2c03ecaecc688fde44b1d1470d4e87f
lukmanhmwn <hermawanlukman231@gmail.com>
2025-11-11 22:44:13 +0800

    fix polimorpisme

--- polimorpisme.js
+++ polimorpisme.js
@@ -1,18 +1,18 @@
-class employee {
-     (){
+class Animals {
+     speak(){
         return console.log("animals make a sound");
     }
 }
 
 class dog extends animals {
-    // speak (){
-    //     return console.log("dog sound like barks");
-    // }
+    speak (){
+        return console.log("dog sound like barks");
+    }
 }
 
 class cat extends animals {
     speak () {
-        return console.log("cat sound like mweow")
+        return console.log("cat sound like meow")
     }
 }
 
